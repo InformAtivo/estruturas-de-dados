2e83412099d5209a7dc91185f1b4ff4521cc6a77
bernardo russo <bfcr@cesar.school>
2018-10-03 14:18:56 -0300

    atualizado com mais comentarios

--- v 0-1/main.c
+++ v 0-1/main.c
@@ -8,31 +8,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct Comentario{
     char texto [260];
     struct Comentario *proximo;
 }comentario;
 
-typedef struct Usario{//deverá haver maior especificação no futuro
+typedef struct{//deverá haver maior especificação no futuro
     char nome[51];
 }usuario;
 
-typedef struct{//Luiz está finalizando esse struct
+typedef struct task{//Luiz está finalizando esse struct
     int tipo;
+    struct task *anterior;
+    struct task *proxima;
+    char desc[260];
 }tarefa;
+
+enum tipos_tarefa {texto, foto, grafico};
 
 typedef struct{
     char titulo[51];
-    char dono[51];
+    char dono[51];//o dono também pode estar numa estrutura de listas
     tarefa *task;
     int deadline;//procurar biblioteca de tempo e funcionamento da mesma
-    comentario *coment;
+    comentario *coment;//lista de comentarios sobre a pauta
 }pauta;
 
-
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {//definir maneiras de colocar valores nas pautas/tarefas
+    
     return 0;
 }
